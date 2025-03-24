42695378732c453610967f2fdd84dff05aae64cb
Yuna <yuna@Yunaui-MacBookPro.local>
2025-03-24 17:45:53 +0900

    Add VPC variable

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -1,4 +1,5 @@
 variable "vpc_main_cidr" {
   description = "VPC main CIDR block"
   type        = string
+  default     = "10.0.0.0/23"
 }
--- variables.tf
+++ variables.tf
@@ -1,24 +1,28 @@
-# variables.tf
+variable "vpc_main_cidr" {
+  description = "VPC main CIDR block"
+  default     = "10.0.0.0/23"
+}
+
 variable "server_port" {
-    description = "Webserver’s HTTP port"
-    type        = number
-    default     = 80
+  description = "Webserver’s HTTP port"
+  type        = number
+  default     = 80
 }
 
 variable "my_ip" {
-    description = "My public IP"
-    type        = string
-    default     = "0.0.0.0/0"
+  description = "My public IP"
+  type        = string
+  default     = "0.0.0.0/0"
 }
 
 variable "alb_security_group_name" {
-    description = "The name of the ALB’s security group"
-    type        = string
-    default     = "webserver-alb-sg-studentN"
+  description = "The name of the ALB’s security group"
+  type        = string
+  default     = "webserver-alb-sg-studentN"
 }
 
 variable "alb_name" {
-    description = "The name of the ALB"
-    type        = string
-    default     = "webserver-alb-studentN"
+  description = "The name of the ALB"
+  type        = string
+  default     = "webserver-alb-studentN"
 }
